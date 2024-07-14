9b39fb3806a06cb33a184949e9528ce21bf45009
Samuel C. Tyler <skamansam@gmail.com>
2024-07-14 13:35:30 -0400

    add tailwind theme and scoped styles for category search

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        "Montserrat",
+        "Romman",
+        "Roboto",
+        "Feeling Passionate",
+        "sans-serif",
+      ],
+      serif: ["Merriweather", "serif"],
+      cursive: ["Dancing Script", "Apple Chancery", "Bradley Hand", "Brush Script MT", "Brush Script Std", "Snell Roundhand", "URW Chancery L", "cursive"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          light: "#FFD700",
+          DEFAULT: "#FFD700",
+          dark: "#FFD700",
+        },
+        secondary: {
+          light: "#FFD700",
+          DEFAULT: "#FFD700",
+          dark: "#FFD700",
+        },
+        white: "rgba(255, 255, 255, 0.87)",
+        black: "#242424",
+        gray: {
+          light: "#D3D3D3",
+          DEFAULT: "#D3D3D3",
+          dark: "#D3D3D3",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import posthtml from '@vituum/vite-plugin-posthtml';
+import path from "node:path";
 
 export default defineConfig({
   plugins: [posthtml({
     root: '',
     include: '**/*.html',
     plugins: [],
-    options: {},
+    options: {
+    },
   })],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, "./src") },
+    ],
+  },
 });
