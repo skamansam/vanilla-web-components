6bc4b2944101171efb585df1de378e4e0d9daa3a
Samuel C Tyler <skamansam@gmail.com>
2026-08-02 17:03:04 -0400

    reformat config files with tabs and update tailwind postcss plugin

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from "vite";
-import posthtml from '@vituum/vite-plugin-posthtml';
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+import posthtml from "@vituum/vite-plugin-posthtml";
+import posthtmlInclude from "posthtml-include";
+import { defineConfig } from "vite";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
-  plugins: [posthtml({
-    root: '',
-    include: '**/*.html',
-    plugins: [],
-    options: {
-    },
-  })],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, "./src") },
-    ],
-  },
+	base: "/vanilla-web-components/",
+	plugins: [
+		posthtml({
+			root: "",
+			include: "**/*.html",
+			plugins: [posthtmlInclude()],
+			options: {},
+		}),
+	],
+	resolve: {
+		alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
+	},
 });
