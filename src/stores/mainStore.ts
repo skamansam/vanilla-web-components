fc2fa11ce28218fd0769ff46021ad98f430460f1
Samuel C Tyler <skamansam@gmail.com>
2026-08-02 17:08:05 -0400

    add type definitions for menu structure and reformat mainStore with tabs and double quotes

--- src/stores/mainStore.ts
+++ src/stores/mainStore.ts
@@ -1,12 +1,26 @@
-import lunchMenu from "../assets/lunchMenu.json";
 import dinnerMenu from "../assets/dinnerMenu.json";
+import lunchMenu from "../assets/lunchMenu.json";
 
+export type MenuCategoryItem = {
+	id?: number;
+	name?: string;
+	ingredients?: string;
+	price?: string;
+	additional?: string;
+};
+
+export type MenuCategory = {
+	description: string;
+	items: Record<string, MenuCategoryItem>;
+};
+
+export type Menu = Record<string, MenuCategory>;
 
 const mainStore = {
-  menu: lunchMenu,
-  setMenu(menuName: string) {
-    this.menu = menuName === "lunch" ? lunchMenu : dinnerMenu;
-  }
-}
+	menu: lunchMenu as Menu,
+	setMenu(menuName: string) {
+		this.menu = menuName === "lunch" ? lunchMenu : dinnerMenu;
+	},
+};
 
 export default mainStore;
